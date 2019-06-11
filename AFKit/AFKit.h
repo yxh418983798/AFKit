1130a0829f8b04de1d177e8425f10854f089d428
Alfie <yxh418983798@dingtalk.com>
2019-06-11 16:37:08 +0800

    Initial Commit

---
+++ AFKit/AFKit.h
@@ -0,0 +1,19 @@
+//
+//  AFKit.h
+//  AFKit
+//
+//  Created by alfie on 2019/6/11.
+//  Copyright © 2019 Alfie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFKit.
+FOUNDATION_EXPORT double AFKitVersionNumber;
+
+//! Project version string for AFKit.
+FOUNDATION_EXPORT const unsigned char AFKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFKit/PublicHeader.h>
+
+
